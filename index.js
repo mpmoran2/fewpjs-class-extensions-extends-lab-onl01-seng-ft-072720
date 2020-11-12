d255bdaf786f80ef25c17bf455eb6a72c4dae9cf
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-11-12 17:01:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+class Polygon {
+
+    constructor(arrayOfSides) {
+        this.sides = arrayOfSides
+    }
+
+    get countSides() {
+        return this.sides.length
+    }
+
+    get perimeter() {
+        const reducer = (accumulator, currentValue) => accumulator + currentValue;
+        return this.sides.reduce(reducer, 0)
+    }
+}
